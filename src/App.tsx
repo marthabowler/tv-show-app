d2f3f04451aa6f19b7ad04955c28450ee3119dfb
Martha Bowler <mbowler133@gmail.com>
2021-10-29 11:18:51 +0100

    Add data and test import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { greet } from "./utils/greet";
+import episodes from "./episodes.json";
 
 function App(): JSX.Element {
+  console.log(`Imported ${episodes.length} episode(s)`);
+  console.log(`First episode's name is ${episodes[0].name}`);
   return <h1>{greet("World")}</h1>;
 }
 
